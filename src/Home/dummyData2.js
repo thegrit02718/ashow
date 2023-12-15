ecaef9678ba963e191822ab507b7a9812dc4d7ff
minseung-gang <strause1@nate.com>
2023-12-15 13:44:28 +0900

    Add: 메인 페이지 더미데이타 추가

---
+++ src/Home/dummyData2.js
@@ -0,0 +1,21 @@
+export const dummyData = [{
+    address:"대구 동구 · 1313세대",
+    name:"용계동 용계역 푸르지오 아츠베르",
+    image: "https://www.yeongnam.com/mnt/file/202106/2021061801000598300023681.jpg",
+    inDate:"2023.10"
+},{
+    address:"대구 수성구 · 146세대",
+    name:"수성동 빌리브 헤리티지",
+    image: "https://img.hankyung.com/photo/202211/01.31686003.1.jpg",
+    inDate:"2023.8"
+},{
+    address:"대구 동구 · 1313세대",
+    name:"용계동 용계역 푸르지오 아츠베르",
+    image: "https://www.yeongnam.com/mnt/file/202106/2021061801000598300023681.jpg",
+    inDate:"2023.10"
+},{
+    address:"대구 수성구 · 146세대",
+    name:"수성동 빌리브 헤리티지",
+    image: "https://img.hankyung.com/photo/202211/01.31686003.1.jpg",
+    inDate:"2023.8"
+},]
